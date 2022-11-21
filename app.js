514786da5075bbe70ae7b2122ac7eb50df4e362e
Abhishek <pundirabhishek987@gmail.com>
2022-11-21 07:20:56 +0530

    import js

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("script is working");
+
+//input
+var name = prompt("enter username");
+
+//processing
+var welcomeMessage = "this script works!! " + name;
+
+//output
+alert(welcomeMessage);
